2f1a6a6c19914f507da388cd90b6c5a4bd45fb50
Muffindude64 <97483337+Muffindude64@users.noreply.github.com>
2022-04-06 13:17:00 -0500

    Add files via upload

---
+++ divorce_project/Sit_1/Sit_2/Sit_4/Sit_7/Sit_fin/sketch.js
@@ -0,0 +1,22 @@
+// let office;
+
+// function preload(){
+//   office = loadImage('../../../../.././divorce_project/Images/office.jpg');
+
+
+// I tried making the backround for this final page an office but for somereason no matter what varient I did of the code above it didn't load in for some reason so I settled with a brown backround
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(139,69,19);
+
+  // image(office, 0, 0, 1500, 900);
+  
+
+  rect(430, 80, 600, 900);
+
+  strokeWeight(20);
+}
